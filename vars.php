288c77aa131c171f9b6059eec164336f764e8437
nooper <nooper>
2007-10-01 20:35:07 +0000

    Now a class

--- vars.php
+++ vars.php
@@ -1,6 +1,16 @@
 <?php
-$dbhost="";
-$dbname="";
-$dbuser="";
-$dbpass="";
+
+class DBclient {
+	private $dbhost="localhost";
+	private $dbname="nooper";
+	private $dbuser="nooper";
+	private $dbpass="nothing";
+	protected $db; 
+	public function __construct()
+	{
+		$sqlstr = "mysql://{$GLOBALS['dbuser']}:{$GLOBALS['dbpass']}@{$GLOBALS['dbhost']}/{$GLOBALS['dbname']}";
+		$this->db = DB::connect($sqlstr);
+		if (DB::isError($this->db)) { die("$sqlstr :: Can't connect: " . $this->db->getMessage( )); }
+	}
+}
 ?>
