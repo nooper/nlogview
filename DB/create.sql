76b92751cf6fb26532b2d6c11350a0a3752dd1ee
nooper <nooper>
2008-11-15 04:29:32 +0000

    Added explore function as SP

--- DB/create.sql
+++ DB/create.sql
@@ -90,3 +90,51 @@ create table nlogview_static
 keyname varchar(255) primary key,
 value text
 ) engine=myisam;
+
+delimiter //
+create procedure explore(IN tmptable varchar(255))
+begin
+declare bitmask int default 7;
+declare nickbit int default 1;
+declare identbit int default 1;
+declare hostbit int default 1;
+set @nicksql = concat('insert into ', tmptable, ' select distinct i.* from nlogview_ircusers i inner join ' , tmptable, ' e on i.userid = e.userid and 
+i.hostid = e.hostid where i.ircuserid not in (select ircuserid from ', tmptable, ')');
+prepare nickquery from @nicksql;
+set @identsql = concat('insert into ', tmptable, ' select distinct i.* from nlogview_ircusers i inner join ' , tmptable, ' e on i.nickid = e.nickid and 
+i.hostid = e.hostid where i.ircuserid not in (select ircuserid from ', tmptable, ')');
+prepare identquery from @identsql;
+set @hostsql = concat('insert into ', tmptable, ' select distinct i.* from nlogview_ircusers i inner join ' , tmptable, ' e on i.userid = e.userid and 
+i.nickid = e.nickid where i.ircuserid not in (select ircuserid from ', tmptable, ')');
+prepare hostquery from @hostsql;
+repeat
+if nickbit = 1 then
+set nickbit = 0;
+execute nickquery;
+if row_count() > 0 then
+set identbit = 1;
+set hostbit = 1;
+end if;
+end if;
+if identbit = 1 then
+set identbit = 0;
+execute identquery;
+if row_count() > 0 then
+set nickbit = 1;
+set hostbit = 1;
+end if;
+end if;
+if hostbit = 1 then
+set hostbit = 0;
+execute hostquery;
+if row_count() > 0 then
+set identbit = 1;
+set nickbit = 1;
+end if;
+end if;
+set bitmask = nickbit + identbit + hostbit;
+until bitmask = 0
+end repeat;
+end//
+delimiter ;
+
